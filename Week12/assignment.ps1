c46ae6511fc1da8eff9f36261cf3f1bdac427a42
jacob <jacob.okoshi@mymail.champlain.edu>
2023-11-29 14:18:36 -0500

    Week 12 Assignment `

---
+++ Week12/assignment.ps1
@@ -0,0 +1,6 @@
+﻿#Login to a remote ssh server
+$address = Read-Host -Prompt "please enter an IP address to connect to"
+$cred = Read-Host -Prompt "please enter credentials"
+New-SSHSession -ComputerName $address -Credential (Get-Credential $cred)
+$the_cmd = read-host -Prompt "Please enter a command"
+(Invoke-SSHCommand -index 0 $the_cmd).Output
